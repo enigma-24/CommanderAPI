dab66da4eb0cb00d38b5c52c0c40c1f4f3d7b584
Aniket <aniket.d2402@gmail.com>
2020-10-06 19:38:07 -0400

    Configuring EFCore in Startup, adding migrations, adding Connection String to create database and table

---
+++ Data/CommanderContext.cs
@@ -0,0 +1,13 @@
+using CommanderAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommanderAPI.Data
+{
+    public class CommanderContext : DbContext
+    {
+        public CommanderContext(DbContextOptions<CommanderContext> options):base(options){}
+
+        public DbSet<Command> Commands { get; set; }
+        
+    }
+}
--- Models/Command.cs
+++ Models/Command.cs
@@ -1,8 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CommanderAPI.Models{
     public class Command{
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(250)]
         public string HowTo { get; set; }
+
+        [Required]
         public string Line { get; set; }
+
+        [Required]
         public string Platform { get; set; }
     }
 }
