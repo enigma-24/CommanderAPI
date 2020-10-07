828ed1add1cff3adf5f845864d2f35537bd000f8
Aniket <aniket.d2402@gmail.com>
2020-10-07 18:37:58 -0400

    Added repo Class to use database instead of MockRepo

---
+++ Data/SqlCommanderRepo.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using System.Linq;
+using CommanderAPI.Models;
+
+namespace CommanderAPI.Data{
+    public class SqlCommanderRepo : ICommanderRepo
+    {
+        private readonly CommanderContext _context;
+
+        public SqlCommanderRepo(CommanderContext context) => _context = context;
+        public IEnumerable<Command> GetAllCommands()
+        {
+            return _context.Commands.ToList();
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return _context.Commands.FirstOrDefault(c => c.Id == id);
+        }
+    }
+}
