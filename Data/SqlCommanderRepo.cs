ddecfe68cb9f704e78f43949aac76efb2444e000
Aniket <aniket.d2402@gmail.com>
2020-10-12 19:30:42 -0400

    Added create, update and savechanges methods

--- Data/SqlCommanderRepo.cs
+++ Data/SqlCommanderRepo.cs
@@ -1,13 +1,24 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using CommanderAPI.Models;
 
-namespace CommanderAPI.Data{
+namespace CommanderAPI.Data
+{
     public class SqlCommanderRepo : ICommanderRepo
     {
         private readonly CommanderContext _context;
 
         public SqlCommanderRepo(CommanderContext context) => _context = context;
+
+        public void CreateCommand(Command cmd)
+        {
+            if (cmd == null)
+                throw new ArgumentNullException(nameof(cmd));
+
+            _context.Commands.Add(cmd);
+        }
+
         public IEnumerable<Command> GetAllCommands()
         {
             return _context.Commands.ToList();
@@ -16,6 +27,16 @@ namespace CommanderAPI.Data{
         public Command GetCommandById(int id)
         {
             return _context.Commands.FirstOrDefault(c => c.Id == id);
+        }
+
+        public bool SaveChanges()
+        {
+            return (_context.SaveChanges() > 0);
+        }
+
+        public void UpdateCommand(Command cmd)
+        {
+            // No implementation
         }
     }
 }
