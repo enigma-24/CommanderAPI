70e4b0718457ad89ee183c6d7dcdb38a702f5909
Aniket <aniket.d2402@gmail.com>
2020-10-12 19:28:20 -0400

    Updated to include create & update command & savechanges

--- Data/ICommanderRepo.cs
+++ Data/ICommanderRepo.cs
@@ -1,9 +1,14 @@
 using System.Collections.Generic;
 using CommanderAPI.Models;
 
-namespace CommanderAPI.Data{
-    public interface ICommanderRepo{
+namespace CommanderAPI.Data
+{
+    public interface ICommanderRepo
+    {
+        bool SaveChanges();
         IEnumerable<Command> GetAllCommands();
         Command GetCommandById(int id);
+        void CreateCommand(Command cmd);
+        void UpdateCommand(Command cmd);
     }
 }
