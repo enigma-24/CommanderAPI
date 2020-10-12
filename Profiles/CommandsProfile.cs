25f2794bf7bd39a77cc927713142f2eb1bd7edd4
Aniket <aniket.d2402@gmail.com>
2020-10-12 19:31:17 -0400

    Mapping added for create & update dtos

--- Profiles/CommandsProfile.cs
+++ Profiles/CommandsProfile.cs
@@ -2,14 +2,16 @@ using AutoMapper;
 using CommanderAPI.Dtos;
 using CommanderAPI.Models;
 
-
 namespace CommanderAPI.Profiles
 {
     public class CommandsProfile:Profile
     {
         public CommandsProfile()
         {
+            // Source => Target
             CreateMap<Command,CommandReadDto>();
+            CreateMap<CommandCreateDto,Command>();
+            CreateMap<CommandUpdateDto,Command>();
         }
     }
 }
